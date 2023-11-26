16e57497ef30914d4b281a2b2d3d88ac1b305193
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-11-26 21:24:20 +0100

    update security group to allow traffic from anywhere

--- secgrp.tf
+++ secgrp.tf
@@ -1,14 +1,14 @@
 resource "aws_security_group" "treten-sg" {
   name        = "treten-sg"
-  description = "Allow TLS inbound traffic"
+  description = "Allow traffic from all sources"
   vpc_id      = aws_vpc.tretenvpc.id
 
   ingress {
     description = "Allow only connection from my ip"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.MYIP]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -19,6 +19,6 @@ resource "aws_security_group" "treten-sg" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "treten-sg"
   }
 }
