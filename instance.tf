c57a981b569217cf45ac164bad44e71e40450a1d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-11-26 18:17:37 +0100

    add instance file for 5 ec2 instances

--- instance.tf
+++ instance.tf
@@ -1,50 +1,31 @@
-resource "aws_key_pair" "ubuntu-key" {
-  key_name   = "ubuntu-key"
-  public_key = file("ubuntukey.pub")
+resource "aws_key_pair" "projectkey" {
+  key_name   = "projectkey"
+  public_key = file("projectkey.pub")
 }
 
 resource "aws_instance" "webtemplate" {
+  count = 5
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.treten-pub-1.id
-  key_name               = aws_key_pair.ubuntu-key.key_name
+  key_name               = aws_key_pair.projectkey.key_name
   vpc_security_group_ids = [aws_security_group.treten-sg.id]
   tags = {
-    Name = "webtemplate"
-  }
-
-  provisioner "file" {
-    source      = "web.sh"
-    destination = "/tmp/web.sh"
+    Name = "node${count.index + 1}"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod u+x /tmp/web.sh",
-      "sudo /tmp/web.sh",
+      "sudo apt update",
+      "sudo apt upgrade",
     ]
   }
 
   connection {
     user        = var.USER
-    private_key = file("ubuntukey")
+    private_key = file("projectkey")
     host        = self.public_ip
   }
-}
-
-resource "aws_ebs_volume" "treten-ebs" {
-  availability_zone = var.ZONE1
-  size              = 3
-
-  tags = {
-    Name = "treten-ebs"
-  }
-}
-
-resource "aws_volume_attachment" "treten_ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.treten-ebs.id
-  instance_id = aws_instance.webtemplate.id
 }
 
 resource "null_resource" "save_output" {
@@ -55,18 +36,8 @@ resource "null_resource" "save_output" {
         echo '${aws_instance.webtemplate.public_ip}' >> output.txt
       EOT
   }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-        echo '${aws_instance.webtemplate.private_ip}' >> output.txt
-      EOT
-  }
 }
 
 output "PublicIP" {
   value = aws_instance.webtemplate.public_ip
-}
-
-output "PrivateIP" {
-  value = aws_instance.webtemplate.private_ip
 }
